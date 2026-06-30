01dd415e8cbb458a3254062c981651119d739838
AlinaBogdanovska <alinkabogdanka@gmail.com>
2026-06-30 20:22:46 +0300

    math_function, rectangle_perimeter, S

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# /ᐠ｡ꞈ｡ᐟ\
+# 2 /ᐠ｡ꞈ｡ᐟ\
 
-from math_functions import rect
+def calculate_rectangle_perimeter(length, width):
+    if length < 0 or width < 0:
+        return "❗Сторони не можуть бути від'ємними!"
+    return 2 * (length + width)
 
-rectangle = rect(8,5)
-
-print(rectangle)
+a = 5.5
+b = 3.2
+S = calculate_rectangle_perimeter(a, b)
+print("Периметр -> ", S)
